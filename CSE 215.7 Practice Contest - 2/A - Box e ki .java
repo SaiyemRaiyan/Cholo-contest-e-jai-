d8628dde6dfc3fadff04d64d41de41af38f52905
Saiyem Raiyan <64364859+SaiyemRaiyan@users.noreply.github.com>
2023-07-29 23:00:36 +0600

    Add files via upload

---
+++ CSE 215.7 Practice Contest - 2/A - Box e ki .java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+ class Main 
+{
+
+      public static void main(String[] args)
+   {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        int a = input.nextInt();
+        int b = input.nextInt();
+        
+        System.out.println(t-a+b);
+    }
+}
